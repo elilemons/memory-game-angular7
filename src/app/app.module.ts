4b2c3861b0dfd17866c67e2d764f8807ab54a54d
Lisa Lemons <llemons@kpmg.com>
2019-04-16 10:11:40 -0400

    Base components wired up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ComponentModule } from './modules/components.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import { CardComponent } from '../modules/card/card.component';
+import { GameComponent } from '../modules/game/game.component';
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    GameComponent
+  ],
+  imports: [],
+  providers: [],
+  bootstrap: []
+})
+
+export class ComponentModule { }
