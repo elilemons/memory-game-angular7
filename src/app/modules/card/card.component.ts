5860bbd1d5d09d32f28527d9b0740c5966c359b6
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-05-14 16:50:45 -0400

    Added angular playground, not working

--- src/app/modules/card/card.component.ts
+++ src/app/modules/card/card.component.ts
@@ -1,15 +1,30 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import Card from '../../models/card';
+import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
   @Input() card: Card;
   @Output() flipped = new EventEmitter<Card>();
 
+
+  ngOnInit() {
+    const IMAGE_DIR: string = '../../assets/images';
+    if (isUndefined(this.card)) {
+      this.card = {
+        id: 1,
+        name: "Mock",
+        image: `${IMAGE_DIR}/kittens/1.png`,
+        isFlipped: true,
+        isMatched: true,
+        order: 1
+      }
+    }
+  }
   handleCardClick(card: Card): void {
     this.flipped.emit(card);
   }
